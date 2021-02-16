cf84dae7c8d2eefb24d013096fd6b07eed9b1e5a
Jede Brahma <brahmajede@gmail.com>
2021-02-16 14:06:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,7 @@ def using_concat
   my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["sports cars", "flatiron school"]
   my_favorite_things.concat(more_favs)
-  my_favorite_things.any? do |num|
-    num > 2
+  my_favorite_things.length
 end
  
     
