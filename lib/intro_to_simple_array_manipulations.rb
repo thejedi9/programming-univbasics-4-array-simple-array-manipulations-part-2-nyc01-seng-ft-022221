954d5ff8f6f21b7affd9d4d75badd0c7be410500
Jede Brahma <brahmajede@gmail.com>
2021-02-16 14:29:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,7 +3,10 @@ def using_concat(array1, array2)
     more_favs = ["sports cars", "flatiron school"]
     @my_favorite_things.concat( more_favs )
 end
- 
+def using_insert(array, item)
+  
+  array.insert(4, item)
+end
     
    
    
