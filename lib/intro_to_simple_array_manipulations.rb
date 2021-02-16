aff941f6f5a81ea8265732b486eeabe6f487ff3f
Jede Brahma <brahmajede@gmail.com>
2021-02-16 14:26:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,4 @@
-
+def using_concat(array1, array2)
   @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
     more_favs = ["sports cars", "flatiron school"]
     @my_favorite_things.concat( [more_favs] )
