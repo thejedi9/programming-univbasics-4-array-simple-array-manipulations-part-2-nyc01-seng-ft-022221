4fa92a322bf8191831d50eb3617d8fc37648b8f7
Jede Brahma <brahmajede@gmail.com>
2021-02-16 15:10:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,7 +18,7 @@ end
   instruments.flatten 
 end 
 def using_delete(array, string)
-  instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+  instructors = %w["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
   no_offense_steven = instructors.delete("Steven")
 end
 def using_delete_at(array, num)
