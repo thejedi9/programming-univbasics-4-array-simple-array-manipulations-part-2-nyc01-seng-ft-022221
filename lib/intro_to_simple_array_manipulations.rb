4b38ce220044107e0162094f979a4bbba31bdcc2
Jede Brahma <brahmajede@gmail.com>
2021-02-16 14:12:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,7 @@
 def using_concat
   @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
     more_favs = ["sports cars", "flatiron school"]
-    @all_my_favs = @my_favorite_things.concat( more_favs )
+    @all_my_favs = @my_favorite_things.concat( [more_favs] )
 end
  
     
