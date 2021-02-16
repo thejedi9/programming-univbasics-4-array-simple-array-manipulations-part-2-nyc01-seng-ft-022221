0944d7d8c4e2a6c31b15b8e01724a72222f7658b
Jede Brahma <brahmajede@gmail.com>
2021-02-16 14:44:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -19,7 +19,7 @@ end
 end 
 def using_delete(array, string)
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-  no_offense_steven = instructors.delete(Steven)
+  instructors.delete(Steven)
 end
 def using_delete_at(array, num)
   famous_robots = ["Johnny 5", "R2D2", "Robocop"]
