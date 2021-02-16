7926c005887e4431f95f5f67be69873e0d76f817
Jede Brahma <brahmajede@gmail.com>
2021-02-16 14:29:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,6 +4,8 @@ def using_concat(array1, array2)
     @my_favorite_things.concat( more_favs )
 end
 def using_insert(array, item)
+  @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+    @another_language = "Python"
   
   array.insert(4, item)
 end
